2b6e81093949e2af64427ff112555eaa9735dca1
Liad <liadmoshe87@gmail.com>
2024-08-17 18:45:55 +0300

    Added functionality for adding segments and points

--- js/math/graph.js
+++ js/math/graph.js
@@ -4,6 +4,38 @@ class Graph {
         this.segments = segments;
     }
 
+    addSegment(seg){
+        this.segments.push(seg);
+    }
+
+    isContainsSegment(seg){
+        return this.segments.find((s) => s.equals(seg));
+    }
+
+    tryAddSegment(seg){
+        if(!this.isContainsSegment(seg) && !seg.p1.equals(seg.p2)){
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
+    addPoint(point){
+        this.points.push(point);
+    }
+
+    containsPoint(point){
+        return this.points.find((p) => p.equals(point));
+    }
+
+    tryAddPoint(point){
+        if(!this.containsPoint(point)){
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
     draw(ctx){
         for(const seg of this.segments){
             seg.draw(ctx);
